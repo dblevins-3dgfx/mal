029384473650d441bf006b7e114700059cc058dc
David Blevins <dblevins@3dgfx.com>
2017-09-17 13:05:32 -0700

    A little cleanup

--- dblev/step0_repl.cpp
+++ dblev/step0_repl.cpp
@@ -1,62 +1,51 @@
 #include <string>
 #include <iostream>
 
-/*
---- step0_repl ----------------------------------
-READ(str): return str
-
-EVAL(ast,env): return ast
-
-PRINT(exp): return exp
-
-rep(str): return PRINT(EVAL(READ(str),""))
-
-main loop: println(rep(readline("user> ")))
-*/
-
 typedef std::string str_t;
 typedef std::string ast_t;
 typedef std::string env_t;
 typedef std::string exp_t;
 
-str_t READ(str_t str);
-exp_t EVAL(ast_t ast, env_t env);
-exp_t PRINT(exp_t exp);
-exp_t rep(str_t str);
+const str_t& READ(const str_t& str);
+const exp_t& EVAL(const ast_t& ast, const env_t& env);
+const exp_t& PRINT(const exp_t& exp);
+const exp_t& rep(const str_t& str);
 
-//////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char* argv[])
+/////////////////////////////////////////////////////////////////////////////////
+int main(int, char**)
 {
+  const std::string prompt("user> ");
   while (true)
   {
-    std::cout << "user> ";
+    std::cout << prompt;
     str_t str;
     std::getline(std::cin, str);
     std::cout << rep(str) << std::endl;
   }
 }
 
-//////////////////////////////////////////////////////////////////////////////////
-str_t READ(str_t str)
+/////////////////////////////////////////////////////////////////////////////////
+const exp_t& rep(const str_t& str)
+{
+  return PRINT(EVAL(READ(str),""));
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+const str_t& READ(const str_t& str)
 {
   return str;
 }
 
-//////////////////////////////////////////////////////////////////////////////////
-exp_t EVAL(ast_t ast, env_t env)
+/////////////////////////////////////////////////////////////////////////////////
+const exp_t& EVAL(const ast_t& ast, const env_t&)
 {
   return ast;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
-exp_t PRINT(exp_t exp)
+const exp_t& PRINT(const exp_t& exp)
 {
   return exp;
 }
 
-//////////////////////////////////////////////////////////////////////////////////
-exp_t rep(str_t str)
-{
-  return PRINT(EVAL(READ(str),""));
-}
 
